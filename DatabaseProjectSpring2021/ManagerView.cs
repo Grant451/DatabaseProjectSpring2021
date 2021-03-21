d7dd8cae5dc503f7aa7f13eed1ef022d4769d5a7
Grant <Grantemc2@att.net>
2021-03-21 16:12:40 -0500

    touched up

--- DatabaseProjectSpring2021/ManagerView.cs
+++ DatabaseProjectSpring2021/ManagerView.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,7 +20,7 @@ namespace DatabaseProjectSpring2021
 
         private void btnSubmit_Click(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
--- DatabaseProjectSpring2021/Properties/Settings.Designer.cs
+++ DatabaseProjectSpring2021/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DatabaseProjectSpring2021.Properties
-{
-
-
+namespace DatabaseProjectSpring2021.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.4.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Cars.mdf;Inte" +
+            "grated Security=True")]
+        public string CarsConnectionString {
+            get {
+                return ((string)(this["CarsConnectionString"]));
             }
         }
     }
