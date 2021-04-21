1ca9fbd1b6ef285fd655f3473aacca9f2e6db620
Grant <Grantemc2@att.net>
2021-04-21 15:50:14 -0500

    cleaned up

--- LocationData/SqlLocationRepository.cs
+++ LocationData/SqlLocationRepository.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using System.Text;
 using DataAccess;
+using LocationData.Models;
 
 namespace LocationData
 {
     class SqlLocationRepository
     {
+        private readonly SqlCommandExecutor executor;
 
+        public SqlLocationRepository(string connectionString)
+        {
+            executor = new SqlCommandExecutor(connectionString);
+        }
     }
 }
